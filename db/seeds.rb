e38bbd43206fc4e72994481b191673d70e16ebca
Soami Charan <charansoami@gmail.com>
2019-07-22 15:28:40 +0530

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,2 @@
 # frozen_string_literal: true
-
-User.create(email: 'charansoami@gmail.com ', password: 'soamic', admin: true)
-User.create(email: 'mukul@kaushik', password: 'kaushik', admin: true)
-User.create(email: 'blazar@solution', password: 'blazar', admin: true)
-User.create(email: 'test@test', password: 'test123', admin: false)
+User.create(email: 'suratpyari@blazarsol.com', password: 'blazarsol', admin: true)
