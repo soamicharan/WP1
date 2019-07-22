f631011d48ffc1e0e8f9059834e04f34d6b22697
soamicharan <charansoami@gmail.com>
2019-07-22 14:58:17 +0530

    deleted

--- db/seeds.rb
+++
@@ -1,3 +0,0 @@
-User.create(:email => "charansoami@gmail.com ", :password => "soamic",:admin => true)
-User.create(:email => "mukul@kaushik", :password => "kaushik",:admin => true)
-User.create(:email => "blazar@solution", :password => "blazar", :admin => true)
